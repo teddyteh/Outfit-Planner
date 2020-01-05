561a566175c0b7e98dd6697b0d5ad4a52f7d2bb8
Teddy <>
2020-01-05 11:54:09 +0800

    Add clothing fixed

--- src/app/clothing.service.ts
+++ src/app/clothing.service.ts
@@ -2,25 +2,36 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Clothing } from './models/clothing';
 import { CLOTHES } from './mock-clothes';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClothingService {
 
-  clothes: Clothing[];
+  // clothes: Clothing[];
+  private clothesCollection: AngularFirestoreCollection<Clothing>;
+  clothes: Observable<Clothing[]> = of([]);
 
-  constructor() {
-    this.clothes = CLOTHES;
-  }
-
-  getClothes(): Observable<Clothing[]> {
-    return of(this.clothes);
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
+    let that = this;
+    this.afAuth.authState.subscribe(authState => {
+      if (authState.uid) {
+        console.log("uid " + authState.uid);
+        that.clothesCollection = afs.collection<Clothing>('clothes', ref => ref.where('user', '==', authState.uid));
+        that.clothes = this.clothesCollection.valueChanges();
+        that.clothes.subscribe(c => console.log(c));
+      }
+    });
   }
 
   addClothing(clothing: Clothing) {
-    clothing.id = this.clothes.length + 1;
-    this.clothes.push(clothing);
+    clothing.user = this.afAuth.auth.currentUser.uid;
+
+    console.log(clothing);
+
+    this.clothesCollection.add({ ...clothing });
   }
 
 }
